8ea124df0d1046fde2371ef85425a314d63a12a0
Rogerio Stranieri <rogeriostran@gmail.com>
2025-06-04 17:45:38 -0300

    atualizado em 04/06/2025 - Rogerio

--- git-diario.ps1
+++ git-diario.ps1
@@ -1,6 +1,18 @@
 git status
 git pull
+
+# Commit e push dentro do submódulo
+Write-Host "Entrando no submódulo PrinceSistemas..."
+cd PrinceSistemas
 git add .
-$msg = Read-Host "Digite a mensagem do commit"
-git commit -m "$msg"
+$msgSub = Read-Host "Digite a mensagem do commit para o submódulo"
+git commit -m "$msgSub"
 git push
+cd ..
+
+# Commit e push no repositório principal
+git add .
+$msgMain = Read-Host "Digite a mensagem do commit para o repositório principal"
+git commit -m "$msgMain"
+git push
+
